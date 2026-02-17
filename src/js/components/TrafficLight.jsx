a248faa70d8b50b5557b4dd1881ea9c49c39b9aa
Marc Caritg <k.kiumbat@gmail.com>
2026-02-17 10:30:11 +0000

    semafor

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const TrafficLight =()=>{
+        return(
+            <main>
+                <div className="p-box"></div>
+                <div className="box-light">
+                    <div className="red"></div>
+                    <div className="yellow"></div>
+                    <div className="green"></div>
+                </div>
+            </main>
+       )
+}
+
+export default TrafficLight
